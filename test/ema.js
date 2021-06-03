ee892a8e1f57678ff46d3d92397dc568a840c991
John <jalibudbud@altavantconsulting.com>
2021-06-03 23:37:14 +0400

    Update entry price

---
+++ test/ema.js
@@ -0,0 +1,19 @@
+const EMA = require('technicalindicators').EMA
+const { Util, Hammer } = require('./../lib/patterns');
+const { _candleData } = require('./../lib/binance');
+const _pair = `DOGE${Util.fiatAsset}`;
+
+const run = async () => {
+  const period = 50;
+  const response = await _candleData(_pair, '1m', period);
+                    
+  const values = response.bars.map(x => {
+    return x.close;
+  });
+  const result = EMA.calculate({period: period, values: values});
+
+  console.log(result);
+
+}
+
+run();
--- test/index.js
+++
@@ -1,7 +0,0 @@
-const EMA = require('technicalindicators').EMA
-
-let period = 8;
-let values = [1,3,4,5,6,7,8,9,10,11,12,13,14,15];                    
-const result = EMA.calculate({period : period, values : values});
-
-console.log(result);
