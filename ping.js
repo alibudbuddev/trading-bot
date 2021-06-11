e1f411c755a00bf6e6690093293092a264d936b4
John <jalibudbud@altavantconsulting.com>
2021-06-11 10:43:11 +0400

    refactor ping

--- ping.js
+++ ping.js
@@ -2,21 +2,11 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const server = http.Server(app);
-const cron = require('node-cron');
 const mongoose = require('mongoose');
 const _ = require('lodash');
 const moment = require('moment-timezone');
 moment.tz.setDefault('Asia/Dubai');
-const detectPattern = require('currency-pattern-detector/build').default;
-const MONGODB_DATABASE = 'mongodb+srv://pmiGTtest:123qwe@cluster0.v5zev.mongodb.net/binance_test?authSource=admin&replicaSet=atlas-jyx4bp-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true';
-const { _binanceCore, _openOrders, _candleData, _assetBalance, _cancelAllOrders } = require('./lib/binance');
-const WalletController = require('./controllers/wallet-controller');
-const OrderBookController = require('./controllers/orderbook-controller');
-const OrderHistoryController = require('./controllers/orderhistory-controller');
-const _fiatAsset = 'BUSD';
-const _altAsset = 'XRP';
-const _pair = `${_altAsset}${_fiatAsset}`;
-const _tpPercentage = 0.005;
+const MONGODB_DATABASE = process.env.MONGODB_DATABASE;
 const bot = require('./bot');
 
 const start = async () => {
