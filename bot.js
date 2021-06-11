f0d870a860752540dc7ef668a3588615c8696e66
John <jalibudbud@altavantconsulting.com>
2021-06-11 09:23:15 +0400

    GTCUSDT

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const WalletController = require('./controllers/wallet-controller');
 const OrderBookController = require('./controllers/orderbook-controller');
 const OrderHistoryController = require('./controllers/orderhistory-controller');
 const _fiatAsset = 'USDT';
-const _altAsset = 'HARD';
+const _altAsset = 'GTC';
 const _pair = `${_altAsset}${_fiatAsset}`;
 const _tpPercentage = 0.005;
 
